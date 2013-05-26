7879dd525276f70062a0cee800ad17c7667f6553
Yuxi Kou <koukou1213@gmail.com>
2013-05-25 21:12:04 -0500

    structure built

--- TwoSum.cc
+++ TwoSum.cc
@@ -16,27 +16,79 @@ using namespace std;
 class Solution
 {
 public:
-  static vector<int> sort(vector<int> &numbers, vector<int>::size_type beg, vector<int>::size_type end)
+  static int swap(vector<int> &numbers, vector<int>::iterator left, vector<int>::iterator right)
   {
-    for( vector<int>::iterator it = numbers.begin() ; it != numbers.end() ; ++ it )
-      
-
+    int tmp = numbers[left];
+    numbers[left] = numbers[right];
+    numbers[right] = tmp;
+    return 1;
   }
-  static vector<int> twoSum(vector<int> &numbers, int target)
+
+  static int sort(vector<int> &numbers, const vector<int>::iterator beg, const vector<int>::iterator end)
   {
-    
+    int pivot;
+    vector<int>::iterator left, right;
+
+    if (end == beg ) return 0;
+
+    left = beg;
+    right = end;
+    pivot = numbers[(left+right)/2];
+
+    while (1)
+    {
+      while (left < right && numbers[left] < pivot ) ++left;
+      while (left < right && numbers[right] > pivot ) --right;
+      if ( left >= right ) break;
+
+      swap(numbers, left, right);
+    }
+
+    if (beg < right) sort(numbers, beg, right);
+    if (lef < end ) sort(numbers, left, end);
+    return 1;
+  }
+
+  static int print(vector<int> &numbers)
+  {
+    for( vector<int>::iterator it = input.begin() ; it !=input.end() ; ++it )
+    {
+      if (it != input.begin()) cout << ",";
+      cout << *it << endl;
+    }
+    return 1;
+  }
+  vector<int> twoSum(vector<int> &numbers, int target)
+  {
+    vector<int>::iterator number1 = numbers.begin();
+    vector<int>::iterator number2 = numbers.end() - 1;
+    vector<int> output;
+    sort(numbers, number1, number2);
+    while(number1 < number2)
+    {
+      if (numbers[number1] + numbers[number2] < target) ++number1;
+      else if (numbers[number1] + numbers[number2] > target) --number2;
+      else
+      {
+        output.push_back(number1-numbers.begin());
+        output.push_back(number2-numbers.begin());
+        break;
+      }
+    }
+    return output;
   }
 };
 
 int main(int argc, char** argv)
 {
-  int temp[] = {1,2,3,4,5,6,7,8};
+  int temp[] = {61,342,23,648,95,26,179,48};
   vector<int> input(temp, temp+sizeof(temp)/sizeof(int)) ;
 
   Solution mySolution;
-
-  for( vector<int>::iterator it = input.begin() ; it !=input.end() ; ++it )
-    cout << *it << endl;
+  mySolution.print(input);
+  vector<int> output = mySolution.twoSum(input, 368);
+  cout<<endl;
+  mySolution.print(output);
   return 0;
 }
 
