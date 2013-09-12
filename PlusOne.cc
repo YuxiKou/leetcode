c76c7214286ea7a603f9b51af7b65dff17f7cb53
Yuxi Kou <koukou1213@gmail.com>
2013-09-11 22:43:23 -0500

    PlusOne a/c

---
+++ PlusOne.cc
@@ -0,0 +1,55 @@
+/*
+  Given a number represented as an array of digits, plus one to the number.
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> plusOne(vector<int> &digits)
+  {
+    bool allNine = true;
+    int length = digits.size();
+    if (length == 0) return digits;
+    for( int i = 0 ; i < length ; ++ i)
+    {
+      if (digits[i] != 9 )
+      {
+        allNine = false;
+        break;
+      }
+    }
+    if (allNine)
+    {
+      vector<int> ret;
+      ret.push_back(1);
+      for( int i = 0 ; i < length ; ++ i)
+      {
+        ret.push_back(0);
+      }
+      return ret;
+    }
+    else
+    {
+      int carry = 0;
+      ++digits[length-1];
+      for( int i = length - 1 ; i > 0 ; -- i)
+      {
+        if (digits[i] == 10)
+        {
+          carry = 1;
+          digits[i] = 0;
+          ++digits[i-1];
+        }
+        else
+        {
+          carry = 0;
+        }
+      }
+    }
+    return digits;
+  }
+};
