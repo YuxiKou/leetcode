d9a7c33e6356407b36c6fdfbd8fa7d31eae3fb96
Yuxi Kou <koukou1213@gmail.com>
2013-09-26 12:15:17 -0500

    MaxSubarray, UniqueBinarySearchTree, PascalTriangle done. result pending

---
+++ PascalTriangle.cc
@@ -0,0 +1,37 @@
+/*
+  Given numRows, generate the first numRows of Pascal's triangle.
+  For example, given numRows = 5,
+  Return
+
+  [
+  [1],
+  [1,1],
+  [1,2,1],
+  [1,3,3,1],
+  [1,4,6,4,1]
+  ]
+*/
+
+#include <iostream>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int> > generate(int numRows)
+  {
+    std::vector<std::vector<int> > ret;
+    ret.push_back(std::vector<int>(1,1));
+    for( int i = 1 ; i < numRows; ++ i)
+    {
+      std::vector<int> oneRow(i,0);
+      oneRow[0] = 1;
+      for( int j = 1 ; j < i - 1 ; ++ j)
+      {
+        oneRow[j] = ret[i-1][j-1] + ret[i-1][j];
+      }
+      oneRow[i-1] = 1;
+      ret.push_back(oneRow);
+    }
+    return ret;
+  }
+};
---
+++ UniqueBinarySearchTree.cc
@@ -0,0 +1,33 @@
+/*
+Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+
+For example,
+Given n = 3, there are a total of 5 unique BST's.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+*/
+#include <iostream>
+#include <vector>
+
+class Solution {
+ public:
+  int numTrees(int n)
+  {
+    std::vector<int> count(n+1,0);
+    int sum;
+    count[0] = 1;
+    count[1] = 1;
+    for (int i = 2 ; i < n + 1 ; ++ i)
+    {
+      for(int j = 0 ; j < i ; ++ j )
+      {
+        count[i] += count[j] * count[i-j-1];
+      }
+              
+    }
+  }
+};
