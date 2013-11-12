1bed34d2ab5aba49bc67d29a10c5ee20e6ad6ad5
Yuxi Kou <koukou1213@gmail.com>
2013-11-12 09:22:45 -0600

    Power ac

--- Power.cc
+++ Power.cc
@@ -2,16 +2,27 @@
   Implement pow(x, n).
 */
 
+#include <iostream>
+
+using namespace std;
+
 class Solution
 {
  public:
   double pow(double x, int n)
   {
-    if(n < 0 ) return 1.0/pow(x, -n);
     if(n == 0) return 1;
     if(n == 1) return x;
-    double ret = pow(x, n/2);
-    if(n % 2 == 0) return ret * ret;
-    else return ret * ret * x;
+    double ret = n < 0 ? pow(x, -n/2) : pow(x, n/2);
+    if(n % 2 == 0) ret *= ret;
+    else ret *= ret * x;
+    if(n < 0) return 1.0/ret;
+    else return ret;
   }
 };
+int main()
+{
+  Solution sol;
+  sol.pow(1.00000, -2147483648);
+  return 0;
+}
