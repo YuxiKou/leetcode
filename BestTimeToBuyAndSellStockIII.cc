fa293d3adb010425b5a16b868fb6c7ad649182be
Yuxi Kou <koukou1213@gmail.com>
2013-11-11 19:24:12 -0600

    BestTimeToBuyAndSellStockIII waiting for compilation

---
+++ BestTimeToBuyAndSellStockIII.cc
@@ -0,0 +1,31 @@
+/*
+  Say you have an array for which the ith element is the price of a given stock on day i.
+  Design an algorithm to find the maximum profit. You may complete at most two transactions.
+  You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+*/
+class Solution
+{
+ public:
+  int maxProfit(vector<int> &prices)
+  {
+    int size = prices.size();
+    if(size < 2) return 0;
+    int min = prices[0];
+    int max = prices[size-1];
+    vector<int> maxPrice(size, 0);
+    int ret = 0;
+    for(int i = 1; i < size; ++i)
+    {
+      if(prices[i] < min) min = prices[i];
+      maxPrice[i] = std::max(maxPrice[i-1], prices[i] - min);
+    }
+
+    for(int i = size - 2; i >= 0; --i)
+    {
+      if(prices[i] > max) max = prices[i];
+      int localMax = maxPrice[i] + std::max(maxPrice[i+1], max - prices[i]);
+      if(localMax > ret) ret = localMax;
+    }
+    return ret;
+  }
+};
