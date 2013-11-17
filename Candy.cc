39da045310e858331ae0000b5da71e0997c75a37
Yuxi Kou <koukou1213@gmail.com>
2013-11-17 11:14:46 -0600

    Candy ac

--- Candy.cc
+++ Candy.cc
@@ -7,50 +7,27 @@
 */
 #include <vector>
 #include <iostream>
+
 class Solution
 {
  public:
   int candy(std::vector<int> &ratings)
   {
-    bool isAsc = true;
-    int length = 0;
-    int sum = ratings.size();
-    for( std::vector<int>::size_type i = 0 ; i < ratings.size() - 1 ; ++ i)
+    int n = ratings.size();
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    std::vector<int> candies(n, 1);
+    for (int i = 1; i <= n - 1; ++ i)
     {
-      if(ratings[i+1] > ratings[i])
-      {
-        if(isAsc)
-        {
-          ++length;
-          sum += length;
-        }
-        else
-        {
-          isAsc = true;
-          length = 0;
-        }
-      }
-      else if(ratings[i+1] < ratings[i])
-      {
-        if(isAsc)
-        {
-          isAsc = false;
-          length = 0;
-        }
-        else
-        {
-          ++length;
-          sum += length;
-        }
-      }
-      else
-      {
-        sum += length;
-      }
+      if (ratings[i] > ratings[i-1]) candies[i] = candies[i-1] + 1;
     }
-    if(isAsc) sum += length;
-    else sum += 1;
-    return sum;
+    for (int i = n - 2; i >= 0; -- i)
+    {
+      if (ratings[i] > ratings[i+1]) candies[i] = std::max(candies[i], candies[i+1] + 1);
+    }
+    int result = 0;
+    for (int i = 0; i < n; ++ i) result += candies[i];
+    return result;
   }
 };
 
