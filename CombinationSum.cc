7c727660dc6e4a9a80addb49e0979d23619d54ce
Yuxi Kou <koukou1213@gmail.com>
2013-11-28 23:17:41 -0600

    CombinationSum ac

--- CombinationSum.cc
+++ CombinationSum.cc
@@ -1,18 +1,16 @@
 /*
-  Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+  Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
 
-  Each number in C may only be used once in the combination.
+  The same repeated number may be chosen from C unlimited number of times.
 
   Note:
   All numbers (including target) will be positive integers.
   Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
   The solution set must not contain duplicate combinations.
-  For example, given candidate set 10,1,2,7,6,1,5 and target 8,
+  For example, given candidate set 2,3,6,7 and target 7,
   A solution set is:
-  [1, 7]
-  [1, 2, 5]
-  [2, 6]
-  [1, 1, 6]
+  [7]
+  [2, 2, 3]
 */
 #include <vector>
 
@@ -21,12 +19,12 @@ using namespace std;
 class Solution
 {
  public:
-  vector<vector<int> > combinationSum2(vector<int> &num, int target)
+  vector<vector<int> > combinationSum(vector<int> &candidates, int target)
   {
-    sort(num.begin(), num.end());
+    sort(candidates.begin(), candidates.end());
     vector<vector<int> > result;
     vector<int> oneRes;
-    combination(result, oneRes, num, 0, target);
+    combination(result, oneRes, candidates, 0, target);
     return result;
   }
   void combination(vector<vector<int> > &result, vector<int> &oneRes, vector<int> &num, int step, int target)
@@ -36,9 +34,8 @@ class Solution
     int n = num.size();
     for(int i = step; i < n; ++ i)
     {
-      if(i != step && num[i] == num[i-1]) continue;
       oneRes.push_back(num[i]);
-      combination(result, oneRes, num, i + 1, target - num[i]);
+      combination(result, oneRes, num, i , target - num[i]);
       oneRes.pop_back();
     }
   }
