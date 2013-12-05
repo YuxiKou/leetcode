084ff6b38cf4b4034b436139f4bd35866dee7f36
Yuxi Kou <koukou1213@gmail.com>
2013-12-04 22:21:05 -0600

    CloneGraph ac

---
+++ CloneGraph.cc
@@ -0,0 +1,65 @@
+/*
+  Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+  OJ's undirected graph serialization:
+  Nodes are labeled uniquely.
+
+  We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
+  As an example, consider the serialized graph {0,1,2#1,2#2,2}.
+
+  The graph has a total of three nodes, and therefore contains three parts as separated by #.
+
+  First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
+  Second node is labeled as 1. Connect node 1 to node 2.
+  Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
+  Visually, the graph looks like the following:
+
+       1
+      / \
+     /   \
+    0 --- 2
+         / \
+         \_/
+*/
+#include <stack>
+#include <map>
+#include <vector>
+
+struct UndirectedGraphNode
+{
+  int label;
+  std::vector<UndirectedGraphNode *> neighbors;
+  UndirectedGraphNode(int x) : label(x) {};
+};
+class Solution
+{
+  std::map<int, UndirectedGraphNode *> nodeMap;
+ public:
+  UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node)
+  {
+    if(node == NULL) return NULL;
+    UndirectedGraphNode *newNode = NULL;
+    std::stack<UndirectedGraphNode *> unCloned;
+    unCloned.push(node);
+    while(!unCloned.empty())
+    {
+      UndirectedGraphNode *curNode = unCloned.top();
+      unCloned.pop();
+      if(curNode == NULL) continue;
+      UndirectedGraphNode *clonedNode = findNode(curNode->label);
+      if(newNode == NULL) newNode = clonedNode;
+      for(std::vector<UndirectedGraphNode *>::iterator it = curNode->neighbors.begin(); it != curNode->neighbors.end(); ++ it)
+      {
+        if(nodeMap[(*it)->label] == NULL) unCloned.push(*it);
+        clonedNode->neighbors.push_back(findNode((*it)->label));
+      }
+    }
+    return newNode;
+  }
+  UndirectedGraphNode *findNode(int value)
+  {
+    if(nodeMap[value] != NULL) return nodeMap[value];
+    UndirectedGraphNode *newNode = new UndirectedGraphNode(value);
+    nodeMap[value] = newNode;
+    return newNode;
+  }
+};
